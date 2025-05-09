e78d698ed0aa36c51eb628f6fb95fc85f30597ee
Nitin Singh <vortechcorp@gmail.com>
2025-05-09 23:28:07 +0530

    created write new-email page

---
+++ src/app/dashboard/write/page.tsx
@@ -0,0 +1,9 @@
+import Write from "@/modules/dashboard/elements/write/write"
+
+function Page() {
+  return (
+    <Write/>
+  )
+}
+
+export default Page
---
+++ src/modules/dashboard/elements/write/write.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { Button } from "@heroui/react";
+import { useState } from "react"
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+import { ICONS } from "@/shared/utils/icons";
+
+function Write() {
+    const [emailTitle, setEmailTitle] = useState("");
+    const [open, SetOpen] = useState(false);
+    const router = useRouter();
+
+    const handleCreate = () => {
+        if(emailTitle.length === 0) {
+            toast.error('Enter the email subject to continue!')
+        } else {
+            const formattedTitle = emailTitle.replace(/\s+/g, "-").replace(/&/g, "-");
+            router.push(`/dashboard/new-email?subject=${formattedTitle}`);
+        }
+    };
+  return (
+    <div className="w-full flex p-5 flex-wrap gap-6 relative">
+        <div className="w-[200px] h-[200px] bg-slate-50 flex flex-col items-center 
+            justify-center rounded border border-gray-200 shadow-sm cursor-pointer"
+            onClick={() => SetOpen(!open)}
+        >
+            <span className="text-2xl block text-center mb-3">{ICONS.plus}</span>
+            <h5 className="text-2xl ">Create New</h5>
+        </div>
+        {
+            open && (
+                <div className="absolute flex items-center justify-center top-0 left-0 bg-[#00000028] h-screen w-full">
+                    <div className="w-[600px] p-5 bg-white rounded shadow relative">
+                        <div className="absolute top-3 right-3">
+                            <span className="text-ld cursor-pointer "
+                                onClick={() => SetOpen(!open)}
+                            >
+                                {ICONS.cross}
+                            </span>
+                        </div>
+                        <h5 className="text-2xl">Enter your Email subject</h5>
+                        <input type="text" 
+                            name="" 
+                            id=""
+                            className="border border-gray-200 w-full my-2 h-[35px] px-2 outline-none"
+                            value={emailTitle}
+                            onChange={(e) => setEmailTitle(e.target.value)} 
+                        />
+                        <Button
+                            color="primary"
+                            className="rounded text-xl mt-3"
+                            onPress={handleCreate}
+                        >
+                            Continue
+                        </Button>
+                    </div>
+                </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default Write
